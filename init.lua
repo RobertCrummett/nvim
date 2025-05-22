28be1c634f94dcac947e8edbb59f9a82eb7c8d8b
Robert Crummett <rncrummett5@gmail.com>
2025-05-22 07:46:53 -0600

    Removed the LSP stuff. Too heavy and slow

--- init.lua
+++ init.lua
@@ -34,47 +34,31 @@ require("lazy").setup({
         end
     },
     {
-        "ibhagwan/fzf-lua",
-        dependencies = {"nvim-tree/nvim-web-devicons"},
-        config = function()
-            local configs = require("fzf-lua")
-
-            configs.setup({
-                git_icons = false,
-                file_icons = false,
-                color_icons = false,
-            })
-        end,
-    },
-    {
         "tpope/vim-fugitive",
     },
     {
-        'Mofiqul/vscode.nvim',
-    },
-    {
         'stevearc/oil.nvim',
-        opts = {},
-        lazy = false,
-    },
-    {
-        'neovim/nvim-lspconfig',
-    },
-    {
-        'mason-org/mason.nvim',
-        config = function()
-            require('mason').setup()
-        end,
-    },
-    {
-        'mason-org/mason-lspconfig.nvim',
-        dependencies = {
-            { 'mason-org/mason.nvim', opts = {} },
-            'neovim/nvim-lspconfig',
+        opts = {
+            columns = {
+                "icon",
+                "permission",
+                "size",
+                "mtime",
+            },
+            delete_to_trash = true,
+            git = {
+                add = function(path)
+                    return true
+                end,
+                mv = function(src_path, dest_path)
+                    return true
+                end,
+                rm = function(path)
+                    return true
+                end,
+            },
         },
-        config = function()
-            require('mason-lspconfig').setup()
-        end,
+        lazy = false,
     },
     {
         'kshenoy/vim-signature',
@@ -118,16 +102,9 @@ require("lazy").setup({
 	{
 		'neomake/neomake',
 	},
-	{
-		'hrsh7th/nvim-cmp'
-	},
-	{
-		'hrsh7th/cmp-nvim-lsp'
-	},
 })
 
 vim.o.termguicolors = true
-
 vim.o.mouse = "a"
 
 vim.o.tabstop = 4
@@ -136,23 +113,8 @@ vim.o.expandtab = true
 
 vim.o.signcolumn = "yes"
 
-vim.o.autoindent = true
-
-vim.cmd [[
-    syntax enable
-    colorscheme lunaperche
-]]
+vim.cmd [[syntax enable]]
 
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<Leader>o", ":Oil<CR>", { desc = "Open file explorer" })
-
-vim.keymap.set("n", "<Leader>ds", vim.diagnostic.open_float, { desc = "Show diagnostic" })
-
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-
-local virtual_text_enabled = true
-vim.keymap.set("n", "<leader>dv", function()
-	vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
-end, { desc = "Toggle diagnostics virtual text" })
