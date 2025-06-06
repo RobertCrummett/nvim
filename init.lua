bfca26966478b5f1b3700d401a670f918663a364
RobertCrummett <rncrummett5@gmail.com>
2025-06-05 18:21:42 -0600

    Minor colorscheme default tweaks

--- init.lua
+++ init.lua
@@ -80,10 +80,10 @@ require('lazy').setup({
 	{
 		'slugbyte/lackluster.nvim',
 		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme lackluster]])
-		end,
+--		priority = 1000,
+--		config = function()
+--			vim.cmd([[colorscheme lackluster]])
+--		end,
 	},
 })
 
