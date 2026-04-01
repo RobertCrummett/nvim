44ed27f3d2f95f62a6b8b95eb50fa5b6f666f0bf
Robert Crummett <robertcrummett@robertcrummett.com>
2026-04-01 10:42:34 -0600

    Updated config for nvim 0.12

--- init.lua
+++ init.lua
@@ -1,96 +1,51 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
-end
-vim.opt.rtp:prepend(lazypath)
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.updatetime = 100
+vim.o.signcolumn = 'no'
 
--- NOTE Disabled because llama.vim will cause stutter when leaders are pressed.
--- vim.g.mapleader = " "
--- vim.g.maplocalleader = "\\"
+vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
 
-require("lazy").setup({
-    spec = {
-        "https://github.com/tpope/vim-fugitive.git",
-        "https://github.com/airblade/vim-gitgutter.git",
-        "https://github.com/ggml-org/llama.vim.git",
-        "https://github.com/ledger/vim-ledger",
-    },
-    install = { colorscheme = { "default" } },
-    checker = { enabled = true },
-})
+vim.pack.add {
+    'https://github.com/neovim/nvim-lspconfig',
+    'https://github.com/stevearc/oil.nvim',
+    'https://github.com/tpope/vim-fugitive.git',
+    'https://github.com/airblade/vim-gitgutter.git',
+    'https://github.com/ggml-org/llama.vim.git',
+    'https://github.com/ledger/vim-ledger',
+}
 
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.updatetime = 100
-vim.opt.signcolumn = "no"
-
--- Highlight todo's just like comments
-vim.api.nvim_set_hl(0, "Todo", { link = "Comment" })
-
--- Language servers
 vim.lsp.config['*'] = {
     capabilities = { textDocument = { semanticTokens = { multilineTokenSupport = true } } },
     root_markers = { '.git' },
 }
-vim.diagnostic.config({ virtual_lines = false })
 
--- Harper spell checking language server
-vim.lsp.config['harper'] = {
-    cmd = { 'harper-ls', '--stdio' },
-    filetypes = { 'markdown', 'tex', 'typst' },
-}
-vim.lsp.enable('harper')
+local function on_jump(diagnostic, bufnr)
+    if not diagnostic then return end
 
--- Lua language server
-vim.lsp.config['luals'] = {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
-    root_markers = { '.luarc.json', '.luarc.jsonc' },
-    settings = { Lua = { runtime = { version = 'LuaJIT' }, diagnostics = { globals = { 'vim' } } } },
-}
-vim.lsp.enable('luals')
+    vim.diagnostic.show(
+        diagnostic.namespace,
+        bufnr,
+        { diagnostic },
+        { virtual_lines = { current_line = true }, virtual_text = false }
+    )
+end
 
--- C language server
-vim.lsp.config['clangd'] = {
-    cmd = { 'clangd' },
-    filetypes = { 'c', 'cpp' },
-    root_markers = { 'compile_commands.json', 'compile_flags.txt' },
-    settings = { clangd = { fallbackFlags = { '-std=c++20' } } },
-}
+vim.keymap.set('n', 'gK', function()
+  local new_config = not vim.diagnostic.config().virtual_lines
+  vim.diagnostic.config({ virtual_lines = new_config })
+end, { desc = 'Toggle diagnostic virtual_lines' })
+
+vim.diagnostic.config({ virtual_lines = false, jump = { on_jump = on_jump } })
+
+vim.lsp.enable('lua_ls')
 vim.lsp.enable('clangd')
 
--- Python language server
-vim.lsp.config['pyright'] = {
-    cmd = { 'pyright-langserver', '--stdio' },
-    filetypes = { 'python' },
+vim.lsp.config['harper_ls'] = {
+    cmd = { 'harper-ls', '--stdio' },
+    filetypes = { 'markdown', 'tex', 'typst' },
+    root_markers = { '.harper-dictionary.txt' },
 }
--- NOTE pyright should be installed through a virtual environment.
--- vim.lsp.enable('pyright')
-
--- Typst language server
-vim.lsp.config['tinymist'] = {
-    cmd = { 'tinymist' },
-    filetypes = { 'typst' },
-}
-vim.lsp.enable('tinymist')
-
--- Open into a scratch buffer by default when no arguments were passed
-if vim.fn.argc() == 0 then
-    vim.opt.shortmess:append("I")
-    vim.opt.buftype = "nofile"
-    vim.opt.bufhidden = "hide"
-    vim.opt.swapfile = false
-end
+vim.lsp.enable('harper_ls')
